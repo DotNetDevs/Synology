7870cddfa1328af1f6d9a54dfc0c07cceea07efe
Matteo Bruni <ar3s@icloud.com>
2015-11-20 12:50:43 +0100

    logout

--- Synology/Auth/AuthRequest.cs
+++ Synology/Auth/AuthRequest.cs
@@ -5,20 +5,29 @@ namespace Synology.Auth
 {
 	public class AuthRequest : SynologyRequest
 	{
-		private readonly Random _rand;
+		string _sessionNumber;
 
 		public AuthRequest(SynologyConnection connection) : base(connection, "auth.cgi", "SYNO.API.Auth", 4)
 		{
-			_rand = new Random((int)DateTime.Now.Ticks);
+			var rand = new Random((int)DateTime.Now.Ticks);
+			_sessionNumber = string.Format("session{0}", rand.Next());
 		}
 
 		public ResultData<AuthResult> Login(string username, string password, string otpCode = null)
 		{
 			var extraLoginParams = !string.IsNullOrWhiteSpace(otpCode) ? string.Format("&otp_code={0}", otpCode) : string.Empty;
-			var additionalParams = string.Format("account={0}&passwd={1}&session=session{2}&format=sid{3}", username, password, _rand.Next() % 1000, extraLoginParams);
+			var additionalParams = string.Format("account={0}&passwd={1}&session={2}&format=sid{3}", username, password, _sessionNumber, extraLoginParams);
 			var url = GetApiUrl("login", additionalParams);
 
 			return Connection.GetDataFromUrl<AuthResult>(url);
+		}
+
+		public ResultData<object> Logout()
+		{
+			var additionalParams = string.Format("session={0}", _sessionNumber);
+			var url = GetApiUrl("logout", additionalParams);
+
+			return Connection.GetDataFromUrl<object>(url);
 		}
 	}
 }
