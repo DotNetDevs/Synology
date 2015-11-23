ec8970c86ac9eb4b1353009a2f289dd3969247c2
Matteo Bruni <ar3s@icloud.com>
2015-11-23 21:01:02 +0100

    fixed download station APIs

--- Synology/DownloadStation/TaskResult.cs
+++ Synology/DownloadStation/TaskResult.cs
@@ -5,12 +5,19 @@ namespace Synology.DownloadStation
 	public class TaskResult
 	{
 		public string Id { get; set; }
+
 		public string Type { get; set; }
-		public int Username { get; set; }
+
+		public string Username { get; set; }
+
 		public string Title { get; set; }
+
 		public string Size { get; set; }
+
 		public string Status { get; set; }
+
 		public TaskAdditionalResult Additional { get; set; }
+
 		[JsonProperty("status_extra")]
 		public TaskStatusExtraResult StatusExtra { get; set; }
 	}
