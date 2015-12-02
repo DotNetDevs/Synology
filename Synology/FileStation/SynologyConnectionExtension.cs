c2c3cd0b330fa852e94d958a08c1a7b782888335
Matteo Bruni <Matteo.Bruni@BASISGROUP.COM>
2015-12-02 13:53:18 +0100

    fixed filestation extension

--- Synology/FileStation/SynologyConnectionExtension.cs
+++ Synology/FileStation/SynologyConnectionExtension.cs
@@ -3,7 +3,7 @@
 	public static class SynologyConnectionExtension
 	{
 		private static FileStationApi _api;
-		public static FileStationApi DownloadStation(this SynologyConnection connection)
+		public static FileStationApi FileStation(this SynologyConnection connection)
 		{
 			return _api ?? (_api = connection.GetApi<FileStationApi>());
 		}
