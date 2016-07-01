b446443596fc39c28757cdfc4d72a40a0a53a689
Matteo Bruni <matteo.bruni@me.com>
2016-07-01 10:11:47 +0200

    fixed nuspec/assemblyinfo

--- Synology/Properties/AssemblyInfo.cs
+++ Synology/Properties/AssemblyInfo.cs
@@ -27,4 +27,4 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.*")]
-//[assembly: AssemblyInformationalVersion("1.0.0-beta")]
+[assembly: AssemblyInformationalVersion("1.0.0-beta")]
