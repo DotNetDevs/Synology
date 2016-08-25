e9d9f143ae521c46cacdb7b0c5c70ed1015492ad
Matteo Bruni <matteo.bruni@me.com>
2016-08-25 14:21:03 +0200

    nuget update to stable packages

--- Synology/Properties/AssemblyInfo.cs
+++ Synology/Properties/AssemblyInfo.cs
@@ -27,4 +27,3 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyInformationalVersion("1.0.0-beta")]
