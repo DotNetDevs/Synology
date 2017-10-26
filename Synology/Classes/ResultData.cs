ebcfbc8ff0317ef5172d8f248dfb84329b292fb3
Matteo Bruni <ar3s@icloud.com>
2017-10-26 13:44:52 +0200

    fixed result classes

--- Synology/Classes/ResultData.cs
+++ Synology/Classes/ResultData.cs
@@ -1,14 +1,19 @@
-﻿namespace Synology.Classes
+﻿using Newtonsoft.Json;
+
+namespace Synology.Classes
 {
 	public class ResultData
 	{
+        [JsonProperty("success")]
 		public bool Success { get; set; }
 
+        [JsonProperty("error")]
 		public ResultError Error { get; set; }
 	}
 
 	public sealed class ResultData<T> : ResultData
 	{
+        [JsonProperty("data")]
 		public T Data { get; set; }
 	}
 }
--- Synology/Classes/ResultError.cs
+++ Synology/Classes/ResultError.cs
@@ -1,7 +1,10 @@
-﻿namespace Synology.Classes
+﻿using Newtonsoft.Json;
+
+namespace Synology.Classes
 {
 	public sealed class ResultError
 	{
+        [JsonProperty("code")]
 		public int Code { get; set; }
 	}
 }
